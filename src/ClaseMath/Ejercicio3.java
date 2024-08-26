695216397b2f86ed4ce4bb5f6d4d4abfb559a57a
jcbdark <josecarlos94aqp@gmail.com>
2024-08-25 21:03:08 -0500

    Clase Math Java

---
+++ src/ClaseMath/Ejercicio3.java
@@ -0,0 +1,16 @@
+package ClaseMath;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        double nota1 = (int) (Math.random()*10);
+        double nota2 = (int) (Math.random()*10);
+        double nota3 = (int) (Math.random()*10);
+        double nota4 = (int) (Math.random()*10);
+        double nota5 = (int) (Math.random()*10);
+        double promedio = (nota1 + nota2 + nota3 + nota4 + nota5)/5;
+        System.out.println("El promedio es: "+ promedio);
+    }
+
+}
